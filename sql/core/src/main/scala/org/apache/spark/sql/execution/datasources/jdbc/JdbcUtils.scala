27c4761ecd816cd6341efd736b224741c03fcae9
kodyzeng <kody.ufo@gmail.com>
2020-05-14 17:05:16 +0800

    MySQL INSERT ON DUPLICATE KEY UPDATE Statement Realization

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/JdbcUtils.scala
@@ -120,7 +120,8 @@ object JdbcUtils extends Logging {
       rddSchema: StructType,
       tableSchema: Option[StructType],
       isCaseSensitive: Boolean,
-      dialect: JdbcDialect): String = {
+      dialect: JdbcDialect,
+      write_mode: String = ""): String = {
     val columns = if (tableSchema.isEmpty) {
       rddSchema.fields.map(x => dialect.quoteIdentifier(x.name)).mkString(",")
     } else {
@@ -142,7 +143,12 @@ object JdbcUtils extends Logging {
       }.mkString(",")
     }
     val placeholders = rddSchema.fields.map(_ => "?").mkString(",")
-    s"INSERT INTO $table ($columns) VALUES ($placeholders)"
+    val updateholders = columns.split(',').map(x => x + "=?").mkString(",")
+    if (write_mode != null && write_mode == "upsert") {
+      s"INSERT INTO $table ($columns) VALUES ($placeholders) ON DUPLICATE KEY UPDATE $updateholders"
+    } else {
+      s"INSERT INTO $table ($columns) VALUES ($placeholders)"
+    }
   }
 
   /**
@@ -574,6 +580,76 @@ object JdbcUtils extends Logging {
           s"Can't translate non-null value for field $pos")
   }
 
+  private type JDBCValueUpsertSetter = (PreparedStatement, Row, Int, Int) => Unit
+
+  private def makeUpsertSetter(
+      conn: Connection,
+      dialect: JdbcDialect,
+      dataType: DataType): JDBCValueUpsertSetter = dataType match {
+    case IntegerType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setInt(pos + 1, row.getInt(rowPos))
+
+    case LongType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setLong(pos + 1, row.getLong(rowPos))
+
+    case DoubleType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setDouble(pos + 1, row.getDouble(rowPos))
+
+    case FloatType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setFloat(pos + 1, row.getFloat(rowPos))
+
+    case ShortType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setInt(pos + 1, row.getShort(rowPos))
+
+    case ByteType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setInt(pos + 1, row.getByte(rowPos))
+
+    case BooleanType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setBoolean(pos + 1, row.getBoolean(rowPos))
+
+    case StringType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setString(pos + 1, row.getString(rowPos))
+
+    case BinaryType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setBytes(pos + 1, row.getAs[Array[Byte]](rowPos))
+
+    case TimestampType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setTimestamp(pos + 1, row.getAs[java.sql.Timestamp](rowPos))
+
+    case DateType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setDate(pos + 1, row.getAs[java.sql.Date](rowPos))
+
+    case t: DecimalType =>
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        stmt.setBigDecimal(pos + 1, row.getDecimal(rowPos))
+
+    case ArrayType(et, _) =>
+      // remove type length parameters from end of type name
+      val typeName = getJdbcType(et, dialect).databaseTypeDefinition
+        .toLowerCase(Locale.ROOT).split("\\(")(0)
+      (stmt: PreparedStatement, row: Row, pos: Int, rowPos: Int) =>
+        val array = conn.createArrayOf(
+          typeName,
+          row.getSeq[AnyRef](rowPos).toArray)
+        stmt.setArray(pos + 1, array)
+
+    case _ =>
+      (_: PreparedStatement, _: Row, pos: Int, rowPos: Int) =>
+        throw new IllegalArgumentException(
+          s"Can't translate non-null value for field $pos")
+  }
+
   /**
    * Saves a partition of a DataFrame to the JDBC database.  This is done in
    * a single database transaction (unless isolation level is "NONE")
@@ -596,7 +672,8 @@ object JdbcUtils extends Logging {
       insertStmt: String,
       batchSize: Int,
       dialect: JdbcDialect,
-      isolationLevel: Int): Iterator[Byte] = {
+      isolationLevel: Int,
+      write_mode: String = ""): Iterator[Byte] = {
     val conn = getConnection()
     var committed = false
 
@@ -634,6 +711,8 @@ object JdbcUtils extends Logging {
       val setters = rddSchema.fields.map(f => makeSetter(conn, dialect, f.dataType))
       val nullTypes = rddSchema.fields.map(f => getJdbcType(f.dataType, dialect).jdbcNullType)
       val numFields = rddSchema.fields.length
+      val upsert_field = rddSchema.fields ++ rddSchema.fields
+      val upsert_setters = upsert_field.map(f => makeUpsertSetter(conn, dialect, f.dataType))
 
       try {
         var rowCount = 0
@@ -641,10 +720,20 @@ object JdbcUtils extends Logging {
           val row = iterator.next()
           var i = 0
           while (i < numFields) {
-            if (row.isNullAt(i)) {
-              stmt.setNull(i + 1, nullTypes(i))
+            if (write_mode != null && write_mode == "upsert") {
+              if (row.isNullAt(i)) {
+                stmt.setNull(i + 1, nullTypes(i))
+                stmt.setNull(i + 1 + numFields, nullTypes(i))
+              } else {
+                upsert_setters(i).apply(stmt, row, i, i)
+                upsert_setters(i + numFields).apply(stmt, row, i + numFields, i)
+              }
             } else {
-              setters(i).apply(stmt, row, i)
+              if (row.isNullAt(i)) {
+                stmt.setNull(i + 1, nullTypes(i))
+              } else {
+                setters(i).apply(stmt, row, i)
+              }
             }
             i = i + 1
           }
@@ -810,7 +899,9 @@ object JdbcUtils extends Logging {
     val batchSize = options.batchSize
     val isolationLevel = options.isolationLevel
 
-    val insertStmt = getInsertStatement(table, rddSchema, tableSchema, isCaseSensitive, dialect)
+    val prop = options.asProperties
+    val write_mode = prop.getProperty("write_mode")
+    val insertStmt = getInsertStatement(table, rddSchema, tableSchema, isCaseSensitive, dialect, write_mode)
     val repartitionedDF = options.numPartitions match {
       case Some(n) if n <= 0 => throw new IllegalArgumentException(
         s"Invalid value `$n` for parameter `${JDBCOptions.JDBC_NUM_PARTITIONS}` in table writing " +
@@ -819,7 +910,7 @@ object JdbcUtils extends Logging {
       case _ => df
     }
     repartitionedDF.rdd.foreachPartition(iterator => savePartition(
-      getConnection, table, iterator, rddSchema, insertStmt, batchSize, dialect, isolationLevel)
+      getConnection, table, iterator, rddSchema, insertStmt, batchSize, dialect, isolationLevel, write_mode)
     )
   }
 
